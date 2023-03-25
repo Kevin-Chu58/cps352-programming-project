1e2c6f5f3b10d09f289e764d17b569e56f0c76de
kevin.chu <kevin.chu@gordon.edu>
2023-03-25 09:53:22 -0400

    Add constraints to tables

--- createdb.sql
+++ createdb.sql
@@ -4,62 +4,82 @@ create variable today date;
 create variable fine_daily_rate_in_cents integer default 5;
 
 create table Category(
-	category_name char(10),
-	checkout_period integer,
-	max_books_out integer
+	category_name char(10) not null,
+	checkout_period integer not null
+		check (checkout_period >= 0),
+	max_books_out integer not null
+		check (max_books_out >= 0),
+	primary key (category_name)
 );
 
 create table Borrower(
-	borrower_id char(10),
-	last_name char(20),
-	first_name char(20),
-	category_name char(10)
+	borrower_id char(10) not null,
+	last_name char(20) not null,
+	first_name char(20) not null,
+	category_name char(10) not null,
+	primary key (borrower_id),
+	foreign key (category_name) references Category(category_name)
 );
 
 create table Borrower_phone(
-    borrower_id char(10),
-    phone char(20)
+    borrower_id char(10) not null,
+    phone char(20) not null,
+	primary key (borrower_id, phone),
+	foreign key (borrower_id) references Borrower(borrower_id)
 );
 
 create table Book_info(
-	call_number char(20),
-	title char(50),
-	format char(2)
+	call_number char(20) not null,
+	title char(50) not null,
+	format char(2) not null,
+	primary key (call_number)
 );
 
 -- The code supplied below for bar_code will cause it to be generated
 -- automatically when a new Book is added to the database
 
 create table Book(
-	call_number char(20),
-	copy_number smallint,
+	call_number char(20) not null,
+	copy_number smallint not null,
 	bar_code integer
-		generated always as identity (start with 1)
+		generated always as identity (start with 1),
+	primary key (call_number, copy_number),
+	unique (bar_code),
+	foreign key (call_number) references Book_info(call_number)
 );
 
 create table Book_author(
-	call_number char(20),
-	author_name char(20)
+	call_number char(20) not null,
+	author_name char(20) not null,
+	primary key (call_number, author_name),
+	foreign key (call_number) references Book_info(call_number)
 );
 
 create table Book_keyword(
-    call_number char(20),
-    keyword varchar(20)
+    call_number char(20) not null,
+    keyword varchar(20) not null,
+	primary key (call_number, keyword),
+	foreign key (call_number) references Book_info(call_number)
 );
 
 create table Checked_out(
-	call_number char(20),
-	copy_number smallint,
-	borrower_id char(10),
-	date_due date
+	call_number char(20) not null,
+	copy_number smallint not null,
+	borrower_id char(10) not null,
+	date_due date not null,
+	primary key (call_number, copy_number),
+	foreign key (call_number, copy_number) references Book(call_number, copy_number),
+	foreign key (borrower_id) references Borrower(borrower_id)
 );
 
 create table Fine(
-	borrower_id char(10),
-	title char(50),
-	date_due date,
-	date_returned date,
-	amount numeric(10,2)
+	borrower_id char(10) not null,
+	title char(50) not null,
+	date_due date not null,
+	date_returned date not null,
+	amount numeric(10,2) not null,
+	primary key (borrower_id, title, date_due),
+	foreign key (borrower_id) references Borrower(borrower_id)
 );
 
 -- This trigger will delete all other information on book if last
